282780c35630e8ae73b4765a04a9946044e199db
rainy-me <github@rainy.me>
2020-12-26 01:28:09 +0900

    fix: sync icon import and change mute icon

--- src/components/FrameItem.vue
+++ src/components/FrameItem.vue
@@ -3,10 +3,25 @@
     <div class="control">
       <div class="close" @click="removeStream(id)">X</div>
       <div class="comment hover" @click="openComment(id)">comment</div>
-      <div class="reload hover" @click="reload()">
+      <div class="reload hover" @click="reload">
         <sync-icon :animate="loading" />
       </div>
-      <div class="mute hover" @click="focusMute(id)">🎧</div>
+      <div class="mute hover" @click="focusMute(id)">
+        <svg
+          class="icon"
+          fill="none"
+          stroke="#ccc"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2.5"
+            d="M15.536 8.464a5 5 0 010 7.072m2.828-9.9a9 9 0 010 12.728M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z"
+          />
+        </svg>
+      </div>
       <a
         class="link"
         :href="`https://www.youtube.com/watch?v=${id}`"
@@ -22,8 +37,12 @@
 <script lang="ts" setup="props">
 import { onMounted, watch, ref } from "vue";
 import { useState } from "../store";
+import SyncIcon from "./SyncIcon.vue";
 
 export default {
+  components: {
+    SyncIcon,
+  },
   props: ["id", "muted"],
 };
 
@@ -149,7 +168,7 @@ export const openComment = (id: string) =>
   cursor: pointer;
 }
 .close:hover {
-  background-color: red;
+  background-color: hotpink;
   color: #fff;
 }
 
@@ -192,7 +211,7 @@ export const openComment = (id: string) =>
 }
 
 .link {
-  background-color: skyblue;
+  background-color: aquamarine;
   padding: 0 10px;
   display: grid;
   border-radius: 10px;
