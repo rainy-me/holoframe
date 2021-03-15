df2390bd2ae2f6bbc3c1e6ce0ffbc0de615b0986
rainy-me <github@rainy.me>
2021-03-16 03:04:13 +0900

    fix: icons been ignored

---
+++ src/icons/index.ts
@@ -0,0 +1,15 @@
+// @ts-nocheck
+export { default as Adjustments } from "./Adjustments.js";
+export { default as Refresh } from "./Refresh.js";
+export { default as ViewGridAdd } from "./ViewGridAdd.js";
+export { default as ViewList } from "./ViewList.js";
+export { default as Collection } from "./Collection.js";
+export { default as ViewBoards } from "./ViewBoards.js";
+export { default as ChevronUp } from "./ChevronUp.js";
+export { default as ChevronDown } from "./ChevronDown.js";
+export { default as ChevronDoubleUp } from "./ChevronDoubleUp.js";
+export { default as ExternalLink } from "./ExternalLink.js";
+export { default as VolumeOff } from "./VolumeOff.js";
+export { default as VolumeUp } from "./VolumeUp.js";
+export { default as XCircle } from "./XCircle.js";
+export { default as Filter } from "./Filter.js";
