ca2c21f7cb29733532510dd2f685f7f1421cba2c
rainy-me <github@rainy.me>
2021-03-16 02:53:50 +0900

    fix: layout css

--- src/store/layout.ts
+++ src/store/layout.ts
@@ -5,19 +5,13 @@ export type Layout = "grid" | "vertical" | "horizontal";
 export const layout = ref<Layout>("grid");
 
 export const layoutStyle = computed(() => {
-  console.log("computing layoutStyle");
   const map: { [k in Layout]: () => CSSProperties } = {
     grid() {
       const count = streamIds.value.length;
-      const gridTemplateRows = Array(Math.ceil(Math.sqrt(count)))
-        .fill("1fr")
-        .join(" ");
-
-      const gridTemplateColumns = count
-        ? Array(Math.ceil(count / Math.ceil(Math.sqrt(count))))
-            .fill("1fr")
-            .join(" ")
-        : "";
+      const gridTemplateRows = `repeat(${Math.ceil(
+        count / Math.ceil(Math.sqrt(count))
+      )},1fr)`;
+      const gridTemplateColumns = `repeat(${Math.ceil(Math.sqrt(count))},1fr)`;
 
       return {
         gridTemplateRows,
